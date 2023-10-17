185732174c4989392f21b0cbc377eafe9552a4c3
GiangLN <giangln@mor.com.vn>
2023-10-17 09:45:08 +0700

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,16 @@
-import type { TabsProps } from 'antd';
-import { Tabs } from 'antd';
 import './App.css';
-import CubeDemo from './CubeDemo';
 import LoadModel from './LoadModel';
-import { useState } from 'react';
 
 function App() {
-  const [activeKey, setActiveKey] = useState<string>('2');
-
-  const onChange = (key: string) => {
-    setActiveKey(key);
-  };
-  
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: 'Cube',
-      children: <CubeDemo />,
-    },
-    {
-      key: '2',
-      label: 'Load Model',
-      children: <LoadModel />,
-    },
-  ];
 
   return (
     <div className="App">
-      <h2>Three JS Demo</h2>
-      <div id="info">
-			"W" translate | "E" rotate | "R" scale | "+/-" adjust size<br />
-			"X" toggle X | "Y" toggle Y | "Z" toggle Z | "Spacebar" toggle enabled<br />
-		</div>
-      {activeKey === '2' ? <div id="debug1"/> : null}
-      <Tabs defaultActiveKey={activeKey} items={items} activeKey={activeKey} onChange={onChange} />
+      <div id="instructions">
+        HEARTVERSE FFD Demo<br />
+        "W" translate | "E" rotate | "R" scale | "+/-" adjust size<br />
+        "X" toggle X | "Y" toggle Y | "Z" toggle Z | "Spacebar" toggle enabled<br />
+      </div>
+      <LoadModel/>
     </div>
   );
 }
--- src/LoadModel.tsx
+++ src/LoadModel.tsx
@@ -13,8 +13,6 @@ const LoadModel = () => {
     const modelRef = useRef<THREE.Group | null>(null);
 
     useEffect(() => {
-        const debug = document.getElementById('debug1') as HTMLDivElement;
-
         // ________________________________________________________________________
         // [Scene]
         const scene: THREE.Scene = new THREE.Scene();
@@ -33,18 +31,10 @@ const LoadModel = () => {
         sceneRef.current?.appendChild(renderer.domElement);
         // renderer.setSize(window.innerWidth/2, window.innerHeight/2, false); Keep the size of the app but render it at a lower resolution
 
-        // Cube
-        // const cube: THREE.Mesh = new THREE.Mesh(
-        //     new THREE.BoxGeometry(5, 5, 5, 3),
-        //     new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-        // );
-        // cube.position.set(0.10191834484203222, -17.518704193074587, 104.21923445137645)
-        // scene.add(cube);
-
         // [Light]
         const topLight = new THREE.DirectionalLight(0xffffff, 0.5); // (color, intensity)
-        topLight.position.set(10, 500, 500) //top-left-ish
-        topLight.castShadow = true;
+        topLight.position.set(1000, 1000, 2000) //top-left-ish
+        // topLight.castShadow = true;
         const ambientLight = new THREE.AmbientLight(0xffffff, 1);
         scene.add(ambientLight);
         scene.add(topLight);
@@ -70,7 +60,6 @@ const LoadModel = () => {
         renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
         renderer.domElement.addEventListener('mousedown', onDocumentMouseDown, false);
         const raycaster = new THREE.Raycaster();
-        const mouse = new THREE.Vector2();
 
         // Load a 3D model (GLTF)
         const loader = new GLTFLoader();
@@ -104,43 +93,6 @@ const LoadModel = () => {
             scene.add(ctrl_pt_mesh);
         }
 
-
-        const segments = 1;
-        function createLatticePoints(controlPoints: THREE.Vector3[], segments: number): THREE.Vector3[] {
-            let latticePoints: THREE.Vector3[] = [];
-
-
-            for (let i = 0; i <= segments; i++) {
-
-                let t = i / segments;
-                let point = new THREE.Vector3();
-
-                point.x = bezierInterpolate(controlPoints.map(p => p.x), t);
-                point.y = bezierInterpolate(controlPoints.map(p => p.y), t);
-                point.z = bezierInterpolate(controlPoints.map(p => p.z), t);
-
-                latticePoints.push(point);
-
-            }
-
-            return latticePoints;
-        }
-
-        function bezierInterpolate(points: number[], t: number): number {
-            const n = points.length - 1;
-            let result = 0;
-
-            for (let i = 0; i <= n; i++) {
-                result += binomialCoefficient(n, i) * Math.pow(1 - t, n - i) * Math.pow(t, i) * points[i];
-            }
-
-            return result;
-        };
-
-        function binomialCoefficient(n: number, k: number): number {
-            if (k === 0 || k === n) return 1;
-            return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
-        }
         // const latticePoints: THREE.Vector3[] = createLatticePoints(mTotalCtrlPtCount, segments);
         // const lattice_line_geom: THREE.BufferGeometry = new THREE.BufferGeometry();
         // const positions = new Float32Array(latticePoints.length * 3);
@@ -157,81 +109,107 @@ const LoadModel = () => {
 
         const lineGeometry = new THREE.BufferGeometry();
         const lineMaterial = new THREE.LineBasicMaterial(({ color: 0x0000ff }))
-        // lineGeometry.setFromPoints([
-        //     mTotalCtrlPtCount[0],
-        //     mTotalCtrlPtCount[1],
-        //     mTotalCtrlPtCount[2],
-        //     mTotalCtrlPtCount[3],
-        //     mTotalCtrlPtCount[4],
-        //     mTotalCtrlPtCount[5],
-        //     mTotalCtrlPtCount[6],
-        //     mTotalCtrlPtCount[17],
-        //     mTotalCtrlPtCount[36],
-        //     mTotalCtrlPtCount[35],
-        //     mTotalCtrlPtCount[34],
-        //     mTotalCtrlPtCount[30],
-        //     mTotalCtrlPtCount[33],
-        //     mTotalCtrlPtCount[32],
-        //     mTotalCtrlPtCount[31],
-        //     mTotalCtrlPtCount[18],
-        //     mTotalCtrlPtCount[0],
-        //     mTotalCtrlPtCount[7],
-        //     mTotalCtrlPtCount[8],
-        //     mTotalCtrlPtCount[1],
-        //     mTotalCtrlPtCount[8],
-        //     mTotalCtrlPtCount[9],
-        //     mTotalCtrlPtCount[2],
-        //     mTotalCtrlPtCount[9],
-        //     mTotalCtrlPtCount[10],
-        //     mTotalCtrlPtCount[3],
-        //     mTotalCtrlPtCount[10],
-        //     mTotalCtrlPtCount[4],
-        //     mTotalCtrlPtCount[11],
-        //     mTotalCtrlPtCount[6],
-        //     mTotalCtrlPtCount[10],
-        //     mTotalCtrlPtCount[11],
-        //     mTotalCtrlPtCount[12],
-        //     mTotalCtrlPtCount[10],
-        //     mTotalCtrlPtCount[13],
-        //     mTotalCtrlPtCount[9],
-        //     mTotalCtrlPtCount[14],
-        //     mTotalCtrlPtCount[8],
-        //     mTotalCtrlPtCount[15],
-        //     mTotalCtrlPtCount[7],
-        //     mTotalCtrlPtCount[16],
-        //     mTotalCtrlPtCount[15],
-        //     mTotalCtrlPtCount[14],
-        //     mTotalCtrlPtCount[13],
-        //     mTotalCtrlPtCount[12],
-        //     mTotalCtrlPtCount[17],
-        //     mTotalCtrlPtCount[23],
-        //     mTotalCtrlPtCount[22],
-        //     mTotalCtrlPtCount[21],
-        //     mTotalCtrlPtCount[20],
-        //     mTotalCtrlPtCount[19],
-        //     mTotalCtrlPtCount[28],
-        //     mTotalCtrlPtCount[27],
-        //     mTotalCtrlPtCount[26],
-        //     mTotalCtrlPtCount[25],
-        //     mTotalCtrlPtCount[24],
-        //     mTotalCtrlPtCount[23],
-
-        // ]);
         lineGeometry.setFromPoints([
+            mTotalCtrlPtCount[18],
+            mTotalCtrlPtCount[0],
+            mTotalCtrlPtCount[1],
+            mTotalCtrlPtCount[7],
+            mTotalCtrlPtCount[1],
+            mTotalCtrlPtCount[2],
+            mTotalCtrlPtCount[8],
+            mTotalCtrlPtCount[2],
+            mTotalCtrlPtCount[3],
+            mTotalCtrlPtCount[9],
+            mTotalCtrlPtCount[3],
+            mTotalCtrlPtCount[4],
+            mTotalCtrlPtCount[10],
+            mTotalCtrlPtCount[4],
+            mTotalCtrlPtCount[5],
+            mTotalCtrlPtCount[11],
+            mTotalCtrlPtCount[5],
+            mTotalCtrlPtCount[6],
+            mTotalCtrlPtCount[17],
+            mTotalCtrlPtCount[11],
+            mTotalCtrlPtCount[10],
+            mTotalCtrlPtCount[9],
+            mTotalCtrlPtCount[8],
             mTotalCtrlPtCount[7],
             mTotalCtrlPtCount[18],
+            mTotalCtrlPtCount[16],
+            mTotalCtrlPtCount[7],
+            mTotalCtrlPtCount[16],
+            mTotalCtrlPtCount[15],
+            mTotalCtrlPtCount[8],
+            mTotalCtrlPtCount[15],
+            mTotalCtrlPtCount[14],
+            mTotalCtrlPtCount[9],
+            mTotalCtrlPtCount[14],
+            mTotalCtrlPtCount[13],
+            mTotalCtrlPtCount[10],
+            mTotalCtrlPtCount[13],
+            mTotalCtrlPtCount[12],
+            mTotalCtrlPtCount[11],
+            mTotalCtrlPtCount[12],
+            mTotalCtrlPtCount[17],
+            mTotalCtrlPtCount[23],
+            mTotalCtrlPtCount[24],
+            mTotalCtrlPtCount[23],
+            mTotalCtrlPtCount[22],
+            mTotalCtrlPtCount[25],
+            mTotalCtrlPtCount[22],
+            mTotalCtrlPtCount[21],
+            mTotalCtrlPtCount[26],
+            mTotalCtrlPtCount[21],
+            mTotalCtrlPtCount[20],
+            mTotalCtrlPtCount[27],
+            mTotalCtrlPtCount[20],
+            mTotalCtrlPtCount[19],
+            mTotalCtrlPtCount[18],
+            mTotalCtrlPtCount[28],
+            mTotalCtrlPtCount[19],
+            mTotalCtrlPtCount[28],
+            mTotalCtrlPtCount[27],
+            mTotalCtrlPtCount[26],
+            mTotalCtrlPtCount[25],
+            mTotalCtrlPtCount[24],
+            mTotalCtrlPtCount[17],
+            mTotalCtrlPtCount[36],
+            mTotalCtrlPtCount[35],
+            mTotalCtrlPtCount[34],
+            mTotalCtrlPtCount[30],
+            mTotalCtrlPtCount[33],
+            mTotalCtrlPtCount[32],
+            mTotalCtrlPtCount[31],
+            mTotalCtrlPtCount[0],
+            mTotalCtrlPtCount[31],
+            mTotalCtrlPtCount[18],
+            mTotalCtrlPtCount[28],
+            mTotalCtrlPtCount[32],
+            mTotalCtrlPtCount[33],
+            mTotalCtrlPtCount[27],
+            mTotalCtrlPtCount[29],
+            mTotalCtrlPtCount[33],
+            mTotalCtrlPtCount[29],
+            mTotalCtrlPtCount[26],
+            mTotalCtrlPtCount[29],
+            mTotalCtrlPtCount[30],
+            mTotalCtrlPtCount[29],
+            mTotalCtrlPtCount[25],
+            mTotalCtrlPtCount[29],
+            mTotalCtrlPtCount[34],
+            mTotalCtrlPtCount[25],
+            mTotalCtrlPtCount[24],
+            mTotalCtrlPtCount[35],
+            mTotalCtrlPtCount[36],
+            mTotalCtrlPtCount[6],
         ]);
         // lineGeometry.setFromPoints(mTotalCtrlPtCount);
         const line = new THREE.Line(lineGeometry, lineMaterial);
-
         scene.add(line);
-        // const lattice = new THREE.Mesh(lineGeometry, lineMaterial);
-        // scene.add(lattice);
-
-
 
         function onDocumentMouseMove(event: any) {
             event.preventDefault();
+            const mouse = new THREE.Vector2();
             mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
             mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
             raycaster.setFromCamera(mouse, camera);
@@ -241,26 +219,28 @@ const LoadModel = () => {
 
             if (intersects.length > 0 && ctrl_pt_mesh_selected != intersects[0].object) {
                 // Temporarily change the cursor shape to a fingering cursor.
-                // if(sceneRef.current){
-                //     sceneRef.current.style.cursor = 'pointer'
-                // }
+                if(sceneRef.current){
+                    sceneRef.current.style.cursor = 'pointer'
+                }
             }
             else {
-                // if (sceneRef.current) {
-                //     sceneRef.current.style.cursor = 'auto'
-                // }
+                if (sceneRef.current) {
+                    sceneRef.current.style.cursor = 'auto'
+                }
             }
         }
 
         function onDocumentMouseDown(event: any) {
-            console.log('down')
             event.preventDefault();
+            console.log('down')
+            const mouse = new THREE.Vector2();
             mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
             mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
             raycaster.setFromCamera(mouse, camera);
             var intersects = raycaster.intersectObjects(ctrl_pt_meshes);
             // If a new control point is selected...
             if (intersects.length > 0 && ctrl_pt_mesh_selected != intersects[0].object) {
+                console.log('here')
                 orbitControls.enabled = false;
                 // If a control point was selected before, detach it from the transform control.
                 if (ctrl_pt_mesh_selected)
@@ -329,21 +309,6 @@ const LoadModel = () => {
 
         function animate() {
             requestAnimationFrame(animate);
-
-            // if (debug) {
-            //     debug.innerText = 'Cube\n' +
-            //         'Local Pos X: ' + cube.position.x.toFixed(4) +
-            //         '\n' +
-            //         'World Pos X: ' + cubeWorldPosition.x.toFixed(4) +
-            //         '\nCube\n' +
-            //         'Local Pos Y: ' + cube.position.y.toFixed(4) +
-            //         '\n' +
-            //         'World Pos Y: ' + cubeWorldPosition.z.toFixed(4) +
-            //         '\nCube\n' +
-            //         'Local Pos Z: ' + cube.position.z.toFixed(4) +
-            //         '\n' +
-            //         'World Pos Z: ' + cubeWorldPosition.z.toFixed(4)
-            // }
             orbitControls.update();
             stats.update();
             render();
@@ -356,16 +321,12 @@ const LoadModel = () => {
 
         return () => {
             window.removeEventListener('resize', handleResize);
-            if (modelRef.current) {
-                scene.remove(modelRef.current);
-                render();
-            }
             sceneRef.current?.removeChild(renderer.domElement);
         }
     }, [])
 
 
-    return <div ref={sceneRef} />
+    return <div ref={sceneRef} id='debug' />
 
 }
 
